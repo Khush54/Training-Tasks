bede0bd276685bea1dc1c374ec752c835e6c3cf6
Khush54 <khushkaur023@gmail.com>
2026-03-03 18:24:21 +0530

    Train Reservation task completed

--- Train Reservation/script.js
+++ Train Reservation/script.js
@@ -67,13 +67,42 @@ function minor() {
     document.getElementById('result_box').innerHTML = output;
 }
 
+function checkGender() {
+    let output = `<h4> Gender Ratio </h4> <br>`;
+    let check_male = passengers_details.filter(i => i.gender == 'M');
+    output += `Male Passengers = ${check_male.length} <br> Female Passengers = ${passengers_details.length - check_male.length}`;
+    document.getElementById('result_box').innerHTML = output;
+}
 
+function addPassengers() {
+    let output = `<h4> Adding Extra Passengers </h4> <br>`;
+    output += `Adding Karan and Anjali... <br>`
+    let updatedPassengerList = [...passengers_details];
+    updatedPassengerList.push({ name: 'Karan', age: 45, gender: 'M', seat: 67 });
+    updatedPassengerList.push({ name: 'Anjali', age: 22, gender: 'F', seat: 21 });
+    for (let i = 0; i < updatedPassengerList.length; i++) {
+        output += `${updatedPassengerList[i].name} | `;
+    }
+    document.getElementById('result_box').innerHTML = output;
+}
+
+function firstPassenger() {
+    let output = `<h4> Separating First Passengers</h4> <br>`;
+    let names = passengers_details.map(i => i.name)
+        const [first, ...rest] = names;
+        output += `First Name : ${first} <br> All Rest Names : ${rest.join(' | ')}`;
+
+    document.getElementById('result_box').innerHTML = output;
+}
 
 document.getElementById('passengerList').addEventListener('click', passenger);
 document.getElementById('seniorCitizens').addEventListener('click', senior_Citizens);
 document.getElementById('uppercaseNames').addEventListener('click', upperCase);
 document.getElementById('totalAgeReduce').addEventListener('click', ageReduce);
 document.getElementById('totalAgeReduceRight').addEventListener('click', ageReduceRight);
-document.getElementById('totalAgeReduceRight').addEventListener('click', ageReduceRight);
 document.getElementById('checkAllAdults').addEventListener('click', adults);
 document.getElementById('checkAnyMinor').addEventListener('click', minor);
+document.getElementById('genderRatio').addEventListener('click', checkGender);
+document.getElementById('addExtraPassengers').addEventListener('click', addPassengers);
+document.getElementById('separateFirstPassenger').addEventListener('click', firstPassenger);
+
